ebd700d396bc402f84e09d45a60a0df52f95f53d
Fyrik <zr7@students.waikato.ac.nz>
2019-09-08 22:47:39 +1200

    Enabled editing customers.

--- FidlyAdvanced2/ViewModels/CustomerFormViewModel.cs
+++ FidlyAdvanced2/ViewModels/CustomerFormViewModel.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace FidlyAdvanced2.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
