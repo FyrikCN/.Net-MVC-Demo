603455ac3b5d31b8e03cc4e4ec103e66a92b7aa7
Fyrik <zr7@students.waikato.ac.nz>
2019-09-22 23:53:36 +1200

    Simple validation: Age must be at least 18 to be on a membership.

--- FidlyAdvanced2/Models/Customer.cs
+++ FidlyAdvanced2/Models/Customer.cs
@@ -9,7 +9,8 @@ namespace FidlyAdvanced2.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
@@ -18,7 +19,10 @@ namespace FidlyAdvanced2.Models
         public MembershipType MembershipType { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? BirthDate { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
