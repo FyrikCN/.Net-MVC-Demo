ba7007dd0fa43441e38711b093a987d2d0ecb00b
Fyrik <zr7@students.waikato.ac.nz>
2019-09-23 23:12:27 +1200

    Refactoring the Customer class; Validation summary.

--- FidlyAdvanced2/Controllers/CustomersController.cs
+++ FidlyAdvanced2/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace FidlyAdvanced2.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
